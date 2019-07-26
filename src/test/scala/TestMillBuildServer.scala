e228d75b6b03d0a71fdedc3f7ce8f1019aeaf12c
Alexandra Dima <alexandra.dima@jetbrains.com>
2019-07-26 17:42:13 +0200

    Wrote more tests for the run, test and scala tets classes requests  for the MillBuildServer

--- src/test/scala/TestMillBuildServer.scala
+++ src/test/scala/TestMillBuildServer.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutionException
 
 import MainBuildClient.config
 import build_client.TestBuildClient
-import ch.epfl.scala.bsp4j.{BuildClientCapabilities, BuildServer, BuildServerCapabilities, BuildTarget, BuildTargetCapabilities, BuildTargetIdentifier, CleanCacheParams, CompileParams, DependencySourcesParams, DiagnosticSeverity, InitializeBuildParams, InitializeBuildResult, InverseSourcesParams, ResourcesParams, RunParams, ScalaBuildServer, ScalaBuildTarget, ScalaMainClassesParams, ScalaPlatform, SourceItem, SourceItemKind, SourcesParams, StatusCode, TestParams, TextDocumentIdentifier}
+import ch.epfl.scala.bsp4j.{BuildClientCapabilities, BuildServer, BuildServerCapabilities, BuildTarget, BuildTargetCapabilities, BuildTargetIdentifier, CleanCacheParams, CompileParams, DependencySourcesParams, DiagnosticSeverity, InitializeBuildParams, InitializeBuildResult, InverseSourcesParams, ResourcesParams, RunParams, ScalaBuildServer, ScalaBuildTarget, ScalaMainClassesParams, ScalaPlatform, ScalaTestClassesItem, ScalaTestClassesParams, ScalaTestParams, SourceItem, SourceItemKind, SourcesParams, StatusCode, TestParams, TextDocumentIdentifier}
 import com.google.gson.JsonObject
 import org.eclipse.lsp4j.jsonrpc.Launcher
 import org.scalatest.{BeforeAndAfterEach, BeforeAndAfterEachTestData, FunSuite, TestData}
@@ -40,11 +40,11 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
      */
     var client: TestBuildClient = _
     val path = "/home/alexandra/mill_exercise/"
-    //val serverStartCommand = Array("../../out/dev/launcher/dest/run", "-i", "mill.contrib.BSP/start")
+    //val serverStartCommand = Array("../out/dev/launcher/dest/run", "-i", "mill.contrib.BSP/start")
     val serverStartCommand = Array(
       "java",
       "-DMILL_CLASSPATH=/home/alexandra/mill-release",
-      "-DMILL_VERSION=0.5.0-54-78f7e4-DIRTYb66f5b63",
+      "-DMILL_VERSION=0.5.0-55-91a48a-DIRTY99ca76d6",
       "-Djna.nosys=true",
       "-cp",
       "/home/alexandra/mill-release",
@@ -55,6 +55,8 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
     val random = new BuildTargetIdentifier(getUri("random/"))
     val foo = new BuildTargetIdentifier(getUri("foo/"))
     val invalidTarget = new BuildTargetIdentifier(getUri("build"))
+    val random_test = new BuildTargetIdentifier(getUri("random/random.test"))
+    val foo_test = new BuildTargetIdentifier(getUri("foo/foo.test"))
     var server: BuildServer with ScalaBuildServer = _
 
     override def afterEach: Unit = {
@@ -67,14 +69,13 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
       server = establishServerConnection(serverStartCommand).asInstanceOf[BuildServer with ScalaBuildServer]
       initializeServer(server)
       server.onBuildInitialized()
-      server.buildTargetCleanCache(new CleanCacheParams(List(mill_exercise, test, random, foo).asJava)).get
+      server.buildTargetCleanCache(new CleanCacheParams(List(mill_exercise, test, random, foo, random_test).asJava)).get
     }
 
     private[this] def establishServerConnection(startServerCommand: Array[String]): BuildServer = {
       val process2 = Runtime.getRuntime.exec(startServerCommand, null, new File(path))
       val inStream = process2.getInputStream
       val outStream = process2.getOutputStream
-      println(process2.isAlive)
 
       val launcher = new Launcher.Builder[BspServer]()
         .setRemoteInterface(classOf[BspServer])
@@ -144,7 +145,7 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
 
     test("verifying that build targets are constructed properly in mill_exercise") {
       val buildTargets = server.workspaceBuildTargets().get.getTargets.asScala
-      assert(buildTargets.length == 6, "Incorrect number of targets")
+      assert(buildTargets.length == 7, "Incorrect number of targets")
       val mill_exercise = buildTargets.filter(t => t.getDisplayName == "mill_exercise").head
       val test = buildTargets.filter(t => t.getDisplayName == "mill_exercise.test").head
       assertMillExercise(mill_exercise)
@@ -253,10 +254,10 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
        "diagnostic was not recorded by the client")
     }
 
-    private[this] def assertProgressNotifications(moduleName: String): Unit = {
+    private[this] def assertProgressNotifications(taskName: String): Unit = {
       assert(client.taskProgresses.nonEmpty, "no progress notifications were sent.")
       assert(client.taskProgresses.exists(
-        notification =>notification.getUnit == moduleName + ".compile"
+        notification =>notification.getUnit == taskName
       ), "no notification for the compile task was sent.")
     }
 
@@ -264,8 +265,8 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
       val compileParams = new CompileParams(List(mill_exercise).asJava)
       compileParams.setOriginId("1000")
       val result = server.buildTargetCompile(compileParams).get
-      assertProgressNotifications("mill_exercise")
-      println(client.diagnostics)
+      assertProgressNotifications("mill_exercise.compile")
+
       assertPublishDiagnostics(mill_exercise, DiagnosticSeverity.ERROR,
                               getUri("mill_exercise/src/AddFile.scala"),
                               1, "1000")
@@ -280,7 +281,7 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
       val compileParams = new CompileParams(List(random).asJava)
       compileParams.setOriginId("10")
       val result = server.buildTargetCompile(compileParams).get
-      assertProgressNotifications("random")
+      assertProgressNotifications("random.compile")
       assertPublishDiagnostics(random, DiagnosticSeverity.INFORMATION,
         getUri("random/"), 1, "10")
       assert(result.getOriginId == "10", "originId was not set correctly")
@@ -292,13 +293,13 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
       compileParams.setOriginId("10")
       compileParams.setArguments(List("-Ywarn-unused").asJava)
       val result = server.buildTargetCompile(compileParams).get
-      println(client.taskProgresses)
+
       assertPublishDiagnostics(random, DiagnosticSeverity.WARNING,
           getUri("random/src/main/scala/RandomClass.scala"), 2, "10")
       assertPublishDiagnostics(random, DiagnosticSeverity.INFORMATION,
           getUri("random/"), 1, "10"
       )
-      assertProgressNotifications("random")
+      assertProgressNotifications("random.compile")
       assert(result.getOriginId == "10", "originId was not set correctly")
       assert(result.getStatusCode == StatusCode.OK, "incorrect status code")
     }
@@ -308,8 +309,7 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
       compileParams.setOriginId("foo-id")
       compileParams.setArguments(List("-Ywarn-unused").asJava)
       val result = server.buildTargetCompile(compileParams).get
-      println(client.taskProgresses)
-      assertProgressNotifications("foo")
+      assertProgressNotifications("foo.compile")
       assert(client.diagnostics.isEmpty, "diagnostics were sent when there was no reason to.")
       assert(result.getOriginId == "foo-id", "originId was not set correctly")
       assert(result.getStatusCode == StatusCode.OK, "incorrect status code")
@@ -352,40 +352,152 @@ class TestMillBuildServer extends FunSuite with BeforeAndAfterEach {
       val params = new RunParams(random)
       params.setOriginId("run")
       val result = server.buildTargetRun(params).get
-      println(client.diagnostics)
       assert(result.getStatusCode == StatusCode.OK, "the server reported the execution was not successful")
       assert(os.exists(os.Path(path) / "random.txt"), "the main class was not ran.")
-      // assert(result.getOriginId == "run", "the server did not set the response origin id") bsp still didn't release
-      // functionality for this
+       assert(result.getOriginId == "run", "the server did not set the response origin id")
     }
 
     test("Testing the run request with parameters - status code OK") {
       val params = new RunParams(random)
-      params.setOriginId("run")
+      params.setOriginId("run2")
       params.setArguments(List("MILL", "BSP").asJava)
       val result = server.buildTargetRun(params).get
       assert(result.getStatusCode == StatusCode.OK, "the server reported the execution was not successful.")
       assert(os.exists(os.Path(path) / "random.txt"), "the main class was not ran.")
-      // assert(result.getOriginId == "run", "the server did not set the response origin id") bsp still didn't release
-      // functionality for this
+       assert(result.getOriginId == "run2", "the server did not set the response origin id")
 
       val file = Source.fromFile(path + "random.txt")
       val lines = file.getLines().toSeq
       assert(lines.length == 2, "the main class did not execute correctly")
-      assert(lines.head == "MILL\n", "wrong execution")
-      assert(lines(1) == "BSP\n", "wrong execution")
+      assert(lines.head == "MILL", "wrong execution")
+      assert(lines(1) == "BSP", "wrong execution")
       file.close()
     }
 
     test("Testing the run request with parameters - status code ERROR") {
       val params = new RunParams(random)
-      params.setOriginId("run")
+      params.setOriginId("run3")
       params.setArguments(List("argument").asJava)
       val result = server.buildTargetRun(params).get
       assert(result.getStatusCode == StatusCode.ERROR, "the server reported the execution went ok.")
-      // assert(result.getOriginId == "run", "the server did not set the response origin id") bsp still didn't release
-      // functionality for this
+       assert(result.getOriginId == "run3", "the server did not set the response origin id")
     }
 
+    test("Testing run request on a module with compile errors - status code ERROR.") {
+      val params = new RunParams(test)
+      params.setOriginId("run4")
+      params.setArguments(List("argument").asJava)
+      val result = server.buildTargetRun(params).get
+      assert(result.getStatusCode == StatusCode.ERROR, "the server reported the execution was not error.")
+      assert(result.getOriginId == "run4", "the server did not set the response origin id")
+    }
 
+    test("Testing test request on a module with compile errors - status code ERROR") {
+      val params = new TestParams(List(test).asJava)
+      params.setOriginId("test")
+      val result = server.buildTargetTest(params).get
+      assert(result.getStatusCode == StatusCode.ERROR, "the server reported the execution was not error.")
+      assert(result.getOriginId == "test", "the server did not set the response origin id")
+    }
+
+    private[this] def assertTestReport(testReport: JsonObject,
+                                       expTarget: BuildTargetIdentifier,
+                                       expFailed: Int, expPassed: Int,
+                                       expIgnored: Int, expCancelled: Int,
+                                       expSkipped: Int): Unit = {
+      assert(testReport.get("failed").getAsInt == expFailed, "incorrect number of failed tests")
+      assert(testReport.get("passed").getAsInt == expPassed, "incorrect number of passed tests")
+      assert(testReport.get("ignored").getAsInt == expIgnored, "incorrect number of ignored tests")
+      assert(testReport.get("cancelled").getAsInt == expCancelled, "incorrect number of cancelled tests")
+      assert(testReport.get("skipped").getAsInt == expSkipped, "incorrect number of skipped tests")
+      assert(testReport.get("target").getAsJsonObject.get("uri").getAsString == expTarget.getUri,
+        "incorrect target Id for this test report")
+    }
+
+    test("testing the test request with no arguments - passed, ignored and failed test cases.") {
+      val params = new TestParams(List(random_test).asJava)
+      params.setOriginId("random-test")
+      val result = server.buildTargetTest(params).get
+      assert(client.taskStarts.count(not => not.getDataKind == "test-started") == 11,
+        "wrong number of task start notifications")
+      assert(client.taskFinishes.count(not => not.getDataKind == "test-finished") == 11,
+        "wrong number of task finish notifications")
+      val testReport = client.taskFinishes.filter(finish => finish.getDataKind == "test-report").head.getData.
+        asInstanceOf[JsonObject]
+      assertTestReport(testReport, random_test, 2, 6, 3, 0, 0)
+      assert(result.getStatusCode == StatusCode.ERROR, "the server reported the execution was not error.")
+      assert(result.getOriginId == "random-test", "the server did not set the response origin id")
+      assertProgressNotifications("random.test.testLocal")
+    }
+
+    test("Testing test request with arguments - only passed test cases.") {
+      val params = new TestParams(List(random_test).asJava)
+      params.setOriginId("random-test2")
+      val scalaParams = new ScalaTestParams()
+      scalaParams.setTestClasses(List(
+        new ScalaTestClassesItem(
+                                random_test,
+                                List("-t", "test3").asJava
+        )).asJava)
+      params.setData(scalaParams)
+      params.setDataKind("scala-test")
+      val result = server.buildTargetTest(params).get
+      assert(client.taskStarts.count(not => not.getDataKind == "test-started") == 2,
+        "wrong number of task start notifications")
+      assert(client.taskFinishes.count(not => not.getDataKind == "test-finished") == 2,
+        "wrong number of task finish notifications")
+      val testReport = client.taskFinishes.filter(finish => finish.getDataKind == "test-report").head.getData.
+        asInstanceOf[JsonObject]
+      assertTestReport(testReport, random_test, 0, 2, 0, 0, 0)
+      assert(result.getStatusCode == StatusCode.OK, "the server reported the execution was not error.")
+      assert(result.getOriginId == "random-test2", "the server did not set the response origin id")
+      assertProgressNotifications("random.test.testLocal")
+    }
+
+    test("Testing test request with arguments - passed and ignored test cases.") {
+      val params = new TestParams(List(random_test).asJava)
+      params.setOriginId("random-test3")
+      val scalaParams = new ScalaTestParams()
+      scalaParams.setTestClasses(List(
+        new ScalaTestClassesItem(
+          random_test,
+          List("-t", "test3",
+              "-t", "test1").asJava
+        )).asJava)
+      params.setData(scalaParams)
+      params.setDataKind("scala-test")
+      val result = server.buildTargetTest(params).get
+      assert(client.taskStarts.count(not => not.getDataKind == "test-started") == 4,
+        "wrong number of task start notifications")
+      assert(client.taskFinishes.count(not => not.getDataKind == "test-finished") == 4,
+        "wrong number of task finish notifications")
+      val testReport = client.taskFinishes.filter(finish => finish.getDataKind == "test-report").head.getData.
+        asInstanceOf[JsonObject]
+      assertTestReport(testReport, random_test, 0, 3, 1, 0, 0)
+      assert(result.getStatusCode == StatusCode.OK, "the server reported the execution was not error.")
+      assert(result.getOriginId == "random-test3", "the server did not set the response origin id")
+      assertProgressNotifications("random.test.testLocal")
+    }
+
+    test("Testing that scala test classes are retrieved correctly - even with two test classes / file.") {
+      val params = new ScalaTestClassesParams(List(random_test, foo_test).asJava)
+      params.setOriginId("scala test classes")
+      val result = server.buildTargetScalaTestClasses(params).get
+      assert(result.getItems.asScala.length == 2, "did not find enough test classes.")
+      val testClassMap = (for (item <- result.getItems.asScala) yield
+        (item.getTarget, item.getClasses.asScala)).toMap
+      assert(testClassMap(random_test).toSet == Seq("RandomTestClass", "RandomTestClass2", "RandomTest2Class").toSet,
+            "test classes not computed correctly")
+      assert(testClassMap(foo_test).toSet == Set("EmptyTestClass"),
+            "test classes not computed correctly")
+    }
+
+    test("Testing that scala test classes can not be computed when there is compilation error.") {
+      val params = new ScalaTestClassesParams(List(test).asJava)
+      params.setOriginId("scala test classes")
+      val result = server.buildTargetScalaTestClasses(params).get
+      assert(result.getItems.asScala.length == 1, "wrong number of targets in the response.")
+      assert(result.getItems.asScala.head.getClasses.asScala.isEmpty,
+        "target does not have any identifiable test classes.")
+    }
 }
